9e5eb5f9cb72969559a919037a92987b34198eb6
Julia Matuzenko <umatuzenko@gmail.com>
2024-09-23 08:25:31 +0200

    Correct spacing and add animations to the Hero section, adjust tailwind config

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,24 +1,52 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const HeroOrbit = ({
   children,
   size,
   rotation,
+  shouldOrbit = false,
+  orbitDuration,
+  shouldSpin = false,
+  spinDuration,
 }: PropsWithChildren<{
   size: number;
   rotation: number;
+  orbitDuration?: string;
+  spinDuration?: string;
+  shouldSpin?: boolean;
+  shouldOrbit?: boolean;
 }>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
       <div
-        className=""
+        className={twMerge(shouldOrbit === true && "animate-spin")}
         style={{
-          transform: `rotate(${rotation}deg)`,
-          height: `${size}px`,
-          width: `${size}px`,
+          animationDuration: orbitDuration,
         }}
       >
-        <div className="inline-flex">{children}</div>
+        <div
+          className="flex items-start justify-start"
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
+          }}
+        >
+          <div
+            className={twMerge(shouldSpin === true && "animate-spin")}
+            style={{ animationDuration: orbitDuration }}
+          >
+            <div
+              className="inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import memojiImage from "@/assets/images/memoji-computer.png";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
@@ -25,34 +24,37 @@ export const HeroSection = () => {
   spinDuration?: string;
   shouldSpin?: boolean;
   shouldOrbit?: boolean;*/}
-        <HeroOrbit size={430} rotation={-14}>
+        <HeroOrbit size={430} rotation={-14} shouldOrbit orbitDuration="30s" shouldSpin spinDuration="3s">
           <SparkleIcon className="size-8 text-violet-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={440} rotation={79}>
+        <HeroOrbit size={440} rotation={79} shouldOrbit orbitDuration="32s" shouldSpin spinDuration="3s">
           <SparkleIcon className="size-5 text-[#8c45ff]/80" />
         </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41}>
+        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
           <div className="size-2 rounded-full bg-purple-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={530} rotation={178}>
+        <HeroOrbit size={530} rotation={178} shouldOrbit orbitDuration="36s" shouldSpin spinDuration="3s">
           <SparkleIcon className="size-10 text-violet-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
+        <HeroOrbit size={550} rotation={20} shouldOrbit orbitDuration="38s" shouldSpin spinDuration="6s">
           <StarIcon className="size-12 text-[#8c45ff]" />
         </HeroOrbit>
-        <HeroOrbit size={590} rotation={98}>
+        <HeroOrbit size={590} rotation={98} shouldOrbit orbitDuration="40s" shouldSpin spinDuration="6s">
           <StarIcon className="size-8 text-sky-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5}>
+        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
           <div className="size-2 rounded-full bg-purple-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={710} rotation={144}>
+        <HeroOrbit size={710} rotation={144} shouldOrbit orbitDuration="44s" shouldSpin spinDuration="3s">
           <SparkleIcon className="size-14 text-sky-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={720} rotation={85}>
+        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
           <div className="size-3 rounded-full bg-purple-400/20" />
         </HeroOrbit>
-        <HeroOrbit size={800} rotation={-72}>
+        <HeroOrbit size={800} rotation={-72} shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="6s">
           <StarIcon className="size-28 text-[#8c45ff]" />
         </HeroOrbit>
       </div>
@@ -94,11 +96,6 @@ export const HeroSection = () => {
             <span>👋</span>
             <span className="font-semibold">Let's Connect</span>
           </button>
-
-          {/* <button className="inline-flex items-center gap-2 border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
-            <span>👋</span>
-            <span className="font-semibold">Let's Connect</span>
-          </button> */}
         </div>
       </div>
     </div>
