6eace197829df12c815d1ad1586ac7211351dcab
Julia Matuzenko <umatuzenko@gmail.com>
2024-09-23 09:09:56 +0200

    Add animation to the Projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrorwUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/grain.jpg";
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -12,9 +14,15 @@ const portfolioProjects = [
     year: "Jun 2023 -",
     title: "Crypto Financial Platform",
     results: [
-      { title: "Built analytics to track key metrics, driving data-based decisions and feature prioritization." },
+      {
+        title:
+          "Built analytics to track key metrics, driving data-based decisions and feature prioritization.",
+      },
       { title: "Increased user retention by 15% through UI/UX improvements." },
-      { title: "Improved landing page performance with Next.js, cutting load times by 20%." },
+      {
+        title:
+          "Improved landing page performance with Next.js, cutting load times by 20%.",
+      },
     ],
     link: "https://kuna.io/en",
     image: kuna,
@@ -24,9 +32,18 @@ const portfolioProjects = [
     year: "2019 - 2022",
     title: "Financial Services",
     results: [
-      { title: "Designed a series of A/B tests to enhance user onboarding and authorization, resulting in a 20% reduction in user drop-off rate." },
-      { title: "Designed 4 different variations of registration flows for different markets according to regulations." },
-      { title: "Migrated a sub-project from Sketch/Zeplin to Figma to enhance team collaboration and speed up feature delivery." },
+      {
+        title:
+          "Designed a series of A/B tests to enhance user onboarding and authorization, resulting in a 20% reduction in user drop-off rate.",
+      },
+      {
+        title:
+          "Designed 4 different variations of registration flows for different markets according to regulations.",
+      },
+      {
+        title:
+          "Migrated a sub-project from Sketch/Zeplin to Figma to enhance team collaboration and speed up feature delivery.",
+      },
     ],
     link: "https://www.exness.uk/",
     image: exness,
@@ -37,7 +54,10 @@ const portfolioProjects = [
     title: "E-Commerce Platform",
     results: [
       { title: "Coded in HTML/SCSS and performed code reviews." },
-      { title: "Improved checkout process to reduce shopping cart abandonment by 15%" },
+      {
+        title:
+          "Improved checkout process to reduce shopping cart abandonment by 15%",
+      },
       { title: "Created clickable prototypes to test user scenarios." },
     ],
     link: "https://rozetka.com.ua",
@@ -49,30 +69,23 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-[#8c45ff] to-sky-400 text-center text-transparent bg-clip-text">
-            Real-world Result
-          </p>
-        </div>
-        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-        Highlighted Projects
-        </h2>
-        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
-          Explore how I turn concepts into engaging digital experiences.
-        </p>
+        <SectionHeader
+          eyebrow="Real-world Result"
+          title=" Highlighted Projects"
+          description="Explore how I turn concepts into engaging digital experiences."
+        />
         <div className="mt-10 gap-20 md:mt-20 flex flex-col ">
-          {portfolioProjects.map((project) => (
-            <div
+          {portfolioProjects.map((project, projectIndex) => (
+            <Card
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20  after:pointer-events-none px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`,
+              }}
             >
-              <div
-                className="absolute inset-0 -z-10 opacity-5"
-                style={{ backgroundImage: `url(${grainImage.src})` }}
-              ></div>
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
-                  <div className="  bg-gradient-to-r from-[#8c45ff] to-sky-400 font-bold inline-flex gap-2 uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <div className="bg-gradient-to-r from-[#8c45ff] to-sky-400 font-bold inline-flex gap-2 uppercase tracking-widest text-sm text-transparent bg-clip-text">
                     <span>{project.company}</span>
                     <span>&bull;</span>
                     <span>{project.year}</span>
@@ -108,7 +121,7 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
