3b4d89c6f1ec6008d35f3bf75582d01cad8e92dd
Julia Matuzenko <umatuzenko@gmail.com>
2024-09-23 22:12:20 +0200

    Fix lint issues

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,7 +10,6 @@ import CssIcon from "@/assets/icons/css3.svg";
 import ReactIcon from "@/assets/icons/react.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GithubIcon from "@/assets/icons/github.svg";
-import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.webp";
 import avatar from "@/assets/images/avatarPhoto.webp";
 import { CardHeader } from "@/components/CardHeader";
